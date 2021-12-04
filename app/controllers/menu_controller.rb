fe4f0aff04d6badcf1dac11a2c3d04d28506318f
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2021-12-04 19:42:09 +0300

    add cart

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
-
 class MenuController < ApplicationController
   before_action :set_cats
   def index
     @page = 'menu'
     @products = Product.all
+    @order_item = current_order.order_items.new
   end
 
   def search
